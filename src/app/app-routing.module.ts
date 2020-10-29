6f1ac6079316085014e57383c0afe63baac5cae4
Shivheras <shivheras@gmail.com>
2020-10-29 22:44:37 +0530

    update code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat-bot/chat/chat.component';
+import { MainComponent } from './chat-bot/main/main.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  
+  
+  {path:'', component: ChatComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat-bot/chat/chat.component';
+import { MainComponent } from './chat-bot/main/main.component';
 
+import {ChatBotModule} from './chat-bot/chat-bot.module';
 @NgModule({
   declarations: [
     AppComponent
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ChatBotModule
   ],
   providers: [],
   bootstrap: [AppComponent]
